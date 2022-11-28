8bf41b9d615f6cd69e461377d997af97bf87961e
nattawatcc <76519685+nattawatcc@users.noreply.github.com>
2022-11-28 17:18:41 +0700

    cnitapon5

---
+++ Labweight/Labweight/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// See https://aka.ms/new-console-template for more information
+
+using Labweight;
+
+Students student1 = new Students();
+student1.name = "sanruethai";
+student1.weight = 55f;
+
+Students student2 = new Students();
+student2.name = "natthawan";
+student2.weight = 57f;
+
+Students student3 = new Students();
+student3.name = "pitchakorn";
+student3.weight = 60f;
+
+Students student4 = new Students();
+student4.name = "witchaphon";
+student4.weight = 70f;
+
+float avgWeight = (student1.weight + student2.weight + student3.weight + student4.weight) / 5;
+Console.WriteLine("Weight avg of Student : " + avgWeight);
+Console.WriteLine("weight high witchaphon");
+Console.WriteLine("weight low sanruethai");
+Console.WriteLine("653450089-1");
---
+++ Labweight/Labweight/Students.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labweight
+{
+    public class Students
+    {
+        public string name;
+        public float weight;
+    }
+}
